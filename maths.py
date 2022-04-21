aff3937b3a170d35e33d16e71b6b0f99f3c99c5a
Prabhat Gupta <gprabhat06@gmail.com>
2022-04-22 00:38:40 +0530

    Section 3.1 Completed

---
+++ maths.py
@@ -0,0 +1,28 @@
+print(2 + 2)
+print(3 - 2)
+print(2 * 2)
+print(2 / 2)
+print("")
+
+print(2 ** 3)
+print(2 // 3)
+print(3 // 3)
+print(4 // 3)
+print("")
+
+# Maths Functions
+print(round(3.3))
+print(round(3.5))
+print("")
+
+print(min(5, 10, 20))
+print(max(5, 10, 20))
+print("")
+
+print(pow(4, 3))
+print(abs(-7.25))
+print("")
+
+#Binary
+print(bin(5))
+print(int('0b101', 2))
