3fef9bf1c23f460884dcec82d6fa97230d552d24
Prabhat Gupta <gprabhat06@gmail.com>
2022-04-22 22:07:08 +0530

    Section 3 completed

--- 13_dict.py
+++ 13_dict.py
@@ -6,7 +6,32 @@ dictionary = {
     'c' : 255,
 }
 
+# print(dictionary)
+# print(dictionary['a'])
+# print(dictionary.keys())
+# print(dictionary.values())
+
+# print(dictionary.get('age'))
+# print(dictionary.get('age', 55))
+
+# user = dict(key = 'value')
+# print(user)
+
+# print('a' in dictionary)
+# print('b' in dictionary.keys())
+# print('d' in dictionary.keys())
+# print('Hello World' in dictionary.values())
+
+# print(dictionary.items())
+# print(dictionary.clear())
+# print(dictionary)
+
+# user = dictionary.copy()
+# print(user)
+
+# print(dictionary.popitem())
 print(dictionary)
-print(dictionary['a'])
-print(dictionary.keys())
-print(dictionary.values())
+print(dictionary.update({'a': 66}))
+print(dictionary)
+print(dictionary.update({'age': 22}))
+print(dictionary)
---
+++ 14_tuples.py
@@ -0,0 +1,13 @@
+tpl = (1,2,3,4,5,2)
+
+# print(tpl[1])
+# print(5 in tpl)
+
+# x = tpl[1]
+# y = tpl[3]
+# print(x)
+# print(y)
+
+print(tpl.count(2))
+print(tpl.index(2))
+print(len(tpl))
