74b9cd41bf91365624e353bf8f6d82112c5352c0
Prabhat Gupta <gprabhat06@gmail.com>
2022-04-27 00:14:37 +0530

    Section 6 Completed

---
+++ Basics 3 - OOP'S/9_mro.py
@@ -0,0 +1,23 @@
+# class A:
+#     pass
+
+# class B(A):
+#     pass
+
+# class C(A):
+#     pass
+
+# class D(B, C):
+#     pass
+
+# print(D.mro())
+# D.__str__
+
+class X: pass
+class Y: pass
+class Z: pass
+class A(X,Y): pass
+class B(Y,Z): pass
+class M(B,A,Z): pass
+
+print(M.mro())
